e5e6f86289928a33be1d67231cbca5911a8a6d11
Azzaid <johanas.azzaid@gmail.com>
2017-09-27 23:28:43 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    var memorizedPart = 0;
+    var previousPartialProduct = "";
+    var twoDigitsProduct = 0;
+    var currentPartialProduct = "";
+    var fullProduct = "";
+    for (decimalShift in second) {
+        for (i in first) {
+            twoDigitsProduct = second[second.length-1-decimalShift] * first[first.length-1-i];
+            twoDigitsProduct += memorizedPart;
+            twoDigitsProduct += (previousPartialProduct[previousPartialProduct.length-i-2]) ? Number(previousPartialProduct[previousPartialProduct.length-i-2]) : 0;
+            currentPartialProduct = String(twoDigitsProduct % 10) + currentPartialProduct;
+            memorizedPart = (twoDigitsProduct - twoDigitsProduct % 10)/10;
+        }
+        if (memorizedPart != 0) {
+            currentPartialProduct = memorizedPart + currentPartialProduct;
+        }
+        fullProduct = currentPartialProduct.slice(currentPartialProduct.length-1, currentPartialProduct.length) + fullProduct;
+        previousPartialProduct = currentPartialProduct;
+        currentPartialProduct = "";
+        memorizedPart = 0;
+    }
+    fullProduct = previousPartialProduct.slice(0,-1) + fullProduct;
+    return (fullProduct);
+};
